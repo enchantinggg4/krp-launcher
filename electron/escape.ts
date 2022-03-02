0e6745fa9297bdbfa1cbf68e192c4798449c6f98
Itachi <enchantinggg4@gmail.com>
2022-03-02 13:03:52 +0300

    fix escaping path

--- electron/escape.ts
+++ electron/escape.ts
@@ -11,14 +11,5 @@ const should_not_escape = (major_release = "", os_build = "") =>
   /1\d+\.\d+/.test(major_release) && Number(os_build) >= 17134.1184;
 
 export function escapePath (given_path: string) {
-  return (is_posix_os)
-    // for posix path, escape is simple
-    ? given_path.replace(/(\s+)/g, '\\$1')
-    // for windows, it depend of the build
-    : (should_not_escape(...windows_version_regex.exec(version)!!.splice(1)))
-      // on major version, no need to escape anymore
-      // https://support.microsoft.com/en-us/help/4467268/url-encoded-unc-paths-not-url-decoded-in-windows-10-version-1803-later
-      ? given_path
-      // on older version, replace space with symbol %20
-      : given_path.replace(/(\s+)/g, '%20');
+  return given_path.replace(/(\s+)/g, '^ ')
 };
